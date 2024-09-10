b0ddf8c09133f2f3548ef205b9497e3a695d1cf1
Counterdoom <davidmanguashvili@gmail.com>
2024-09-10 19:08:16 +0400

    Create information.py

---
+++ homework/Day 1/information.py
@@ -0,0 +1,19 @@
+name = "david"
+
+surname = "manguashvili"
+
+print(name)
+
+name = "david"
+age = 14
+height = "152.0cm"
+
+knows_programming = False
+is_ugly = False
+
+sentence = "მე ვარ დავით მანგუაშვილი, 14 წლის."
+
+print(name + " " + surname)
+print(name + " " + str(age))
+print(name + " " + str(height))
+print(name + " " + str(sentence))
