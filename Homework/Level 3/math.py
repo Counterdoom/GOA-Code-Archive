fe4430c8df32f7e1e3d6f38897ff6d863b186027
Counterdoom <davidmanguashvili@gmail.com>
2024-09-24 17:54:01 +0400

    Create math.py

---
+++ Homework/Level 3/math.py
@@ -0,0 +1,34 @@
+# plus
+print(20 + 50)
+print(10 + 11)
+print(1.5 + 9.6)
+print(7.1 + 12)
+print(99 + 0.6)
+
+# minus
+print(40 - 25)
+print(22.5 - 9)
+print(52.2 - 51.2)
+print(22 - 19.9)
+print(11 - 10)
+
+# multiply
+print(60 * 3)
+print(2.7 * 9)
+print(26 * 4)
+print(30 * 4.5)
+print(22.2 * 2)
+
+# divide
+print(60 / 3)
+print(72 / 8)
+print(75 / 15)
+print(360 / 72)
+print(999 / 166.5)
+
+# exponent
+print(7 ** 3)
+print(2 ** 14)
+print(9 ** 6)
+print(2 ** 17)
+print(16 ** 9)
