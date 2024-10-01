806ee754f4d10c40fddcf22b0f72319e5026fa2c
Counterdoom <davidmanguashvili@gmail.com>
2024-10-01 17:29:47 +0400

    Create hw4.py

---
+++ Level 4/hw/hw4.py
@@ -0,0 +1,20 @@
+# homework 1
+user_age = 14
+surname = razmadze
+print(str(surname) + str(user_age))
+
+# homework 2
+user_age = 14
+print(int(user_age) + 10)
+
+# homework 3
+Num1 = 16
+Num2 = 36
+Num3 = 29
+Num4 = 10
+Num5 = 40
+print((16+36+29+10+40)/5)
+
+# homework 4
+# Case Sensitive არის როდესაც პროგრამირების ენას ფიქრობს რომ დიდი და პატარა ასოები სხვადასხვა სიმბოლოებია (a A)
+# Snake Case არის სიტყვების _ სიმბოლოთი დაშორება
