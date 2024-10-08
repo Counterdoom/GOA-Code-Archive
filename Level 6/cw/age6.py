abc1a42ba4a5c7c702ab0b786af9a48e21af1531
Counterdoom <davidmanguashvili@gmail.com>
2024-10-08 22:35:41 +0400

    Create age6.py

---
+++ Level 6/cw/age6.py
@@ -0,0 +1,18 @@
+# cw1
+age = 14
+print(14 // 10)
+
+# cw2
+age = 14
+print(type(age))
+age2 = 14.0
+print(type(age2))
+
+# cw3
+name = david
+number = 35
+print(name * 35)
+
+# cw4
+# explicit არის ჩვენი ხელით მონაცემთა ტიპის შეცვლა
+# implicit არის ავტომატური ტიპის შეცვლა
