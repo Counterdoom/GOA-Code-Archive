ca85e055b4693a38aa5dbfe9705362e3d62d67f1
Counterdoom <davidmanguashvili@gmail.com>
2024-10-15 18:52:30 +0400

    Update and rename (placeholder) to homework6.py

---
+++ Level 6/hw/homework6.py
@@ -0,0 +1,32 @@
+#hw1
+user_age = 24
+age_10 = float(user_age + 10)
+print(age_10)
+print(type(age_10))
+
+#hw2
+surname = "manguashvili"
+print(surname * 100)
+
+#hw3
+print("არა" + "რეალური")
+print("ავტო" + "ბუსი")
+print("კომპი" + "უტერი")
+print("მათე" + "მატიკა")
+print("კონკატ" + "ინაცია")
+
+#hw4
+# სტრინგი "str" გამოგვადგება სიტყვების შესანახად.
+# ინტეგერი "int" გვჭირდება სრული რიცხვების შესანახად.
+# ფლოატი "float" გვჭირდება წილადების და შერეული რიცხვების შესანახად.
+# ინტეგერისა და ფლოატით მათემატიკური ოპერავიები შეგბიძლია, მაგრამ სტრინგის არა.
+
+#hw5
+system = "Windows 10"
+users = 1000000000
+nt_version = 10.0
+update = "22H2"
+print(type(system))
+print(type(users))
+print(type(nt_version))
+print(type(update))
