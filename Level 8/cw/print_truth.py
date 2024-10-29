a9807292453c65a2bfe1fbd49b02b32cb6080dff
Counterdoom <davidmanguashvili@gmail.com>
2024-10-29 16:42:11 +0400

    Create print_truth.py

---
+++ Level 8/cw/print_truth.py
@@ -0,0 +1,20 @@
+#hw1
+# or - თუ წერია True or True და True or False, მივიღებთ True-ს.
+# and - უნდა მხოლოდ იყო True and True.
+
+#hw2
+print(True or True) # True
+print(True or False) # True
+print(False or True) # True
+print(False or False) # False
+ 
+print(True and True) # True and True
+print(True and False) # True and False
+print(False and True) # False and True
+print(False and False) # False and False
+
+#hw3
+name = "saba"
+age = 27
+result = age > 18
+print(result)
