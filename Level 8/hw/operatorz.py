9b14fdedb61345d9d4a2175b02d61205ad8bb630
Counterdoom <davidmanguashvili@gmail.com>
2024-10-29 16:52:08 +0400

    Create operatorz.py

---
+++ Level 8/hw/operatorz.py
@@ -0,0 +1,16 @@
+#hw1
+# <unfinished>
+
+#hw2
+my_number = 73
+he_number = 99
+result = my_number = he number
+print(result)
+
+#hw3
+# True or False გამოიტანს True-ს რადგან ერთერთი ცვლადი არის True
+# 5 > 3 გამოიტანს True რადგან ეს სწორი შედარებაა
+# "name" == "name" გამოიტანს True-ს რადგან ორივე string ერთნაირია
+# 5 >= 5 გამოიტანს True-ს რადან ეს სწორი შედარებაა
+
+#hw4
