67d89efb4f1731f5e2b6c56f832cd2af27675bc8
Counterdoom <davidmanguashvili@gmail.com>
2024-11-18 19:41:33 +0400

    Update and rename (placeholder) to cw9.py

---
+++ Level 9/cw/cw9.py
@@ -0,0 +1,25 @@
+#cw1
+for i in range(51, 149, 2):
+     print(i)
+
+sentence = "It is a car"
+for i in sentence:
+     print(i)
+
+#cw2
+i = 500
+while i < 700:
+    print(i)
+    i = i + 2
+
+i = 50
+while i < 0:
+     print(i)
+     i = i - 1
+
+#cw3
+favor = 75
+other = 10
+while other < 75:
+     print(other)
+     other = other + 1
