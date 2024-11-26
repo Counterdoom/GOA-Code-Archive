57487157fd9ed4738f8aa1446d34c7c133a5eacb
Counterdoom <davidmanguashvili@gmail.com>
2024-11-26 21:50:54 +0400

    Create list.py

---
+++ Level 12/cw/list.py
@@ -0,0 +1,12 @@
+#hw1
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+index = int(input(8))
+print("Element at index " + str(index) + ": " + str(my_list[index]))
+
+
+#hw2
+names = ["John", "Jane", "Alice", "Bob", "Charlie"]
+index = int(input(4))
+new_name = "David"
+names[index] = new_name
+print("Updated list:" + names)
