addcf6d0ca4202c871852c969da92ec1400b0821
Counterdoom <davidmanguashvili@gmail.com>
2024-12-10 20:33:49 +0400

    Create hw14.py

---
+++ Level 14/hw/hw14.py
@@ -0,0 +1,29 @@
+# hw1
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+print(days[0], days[-1])
+days[2] = input("Nextday")
+print(days)
+
+# hw2
+animals = ["Dog", "Cat", "Elephant", "Tiger", "Lion"]
+print(animals[0], animals[-1])
+animals[4] = input("Giraffe")
+print(animals)
+
+# hw3
+numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+print(numbers1[::3])
+print(numbers1[::-1])
+
+# hw4
+divisible = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000]
+print(divisible[:6])
+print(divisible[::3])
+
+# hw5
+numbers2 = [5, 8, 12, 6, 9, 3, 7, 15, 20, 10]
+print(numbers2[:4])
+middle = numbers2[3:7]
+print(middle_four)
+result = numbers2[-1] == 10
+print(result)
