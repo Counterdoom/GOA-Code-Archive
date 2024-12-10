2d360a3850d815bdcffa218f096c853d5117d62d
Counterdoom <davidmanguashvili@gmail.com>
2024-12-10 20:34:31 +0400

    Create cw14.py

---
+++ Level 14/cw/cw14.py
@@ -0,0 +1,24 @@
+#cw1
+list_a = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k"]
+user_num1 = 4
+user_num2 = 8
+print(list_a[user_num1:user_num2])
+
+#cw2
+list_b = [4, 3, 2, 1, 0]
+user_num3 = -3
+print(list_b[user_num3])
+
+#cw3
+user_name = "andria"
+my_name = "daviti"
+print(my_name[0:3]+user_name[5:7])
+
+#cw4
+#ინდექსირება მიუთითებს კონკრეტულ ელემენტზე სიაში, მაგალითად, list[0] გადასცემს პირველ ელემენტს.
+#Slicing არის სერია ელემენტების, და გამოიყოფა slice[start:end] ფორმატით.
+
+#cw5
+user_name2 = "abshilava"
+user_name3 = user_name2[::-1]
+print(user_name3)
