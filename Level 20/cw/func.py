b4bcd4fd543728f1b23a094e5fb1f73e92cc3b57
Counterdoom <davidmanguashvili@gmail.com>
2025-02-03 16:03:48 +0400

    Create func.py

---
+++ Level 20/cw/func.py
@@ -0,0 +1,28 @@
+#001
+def string_to_number(s):
+    return int(s)
+
+#002
+def boolean_to_string(b):
+    return str(b)
+
+#003
+def make_upper_case(s):
+    return s.upper()
+
+#004
+def bool_to_word(boolean):
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
+
+#005
+def make_negative( number ):
+    if number >= 0:
+       return 0 - number
+    else:
+        return 0 + number
+
+#006
+[?]
