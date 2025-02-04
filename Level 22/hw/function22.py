2ac931af2c1accaa0c8542ec979658a91b45dadf
Counterdoom <davidmanguashvili@gmail.com>
2025-02-04 20:56:02 +0400

    Create function22.py

---
+++ Level 22/hw/function22.py
@@ -0,0 +1,18 @@
+#001
+def odd_or_even(numbers):
+  if number in numbers % 2 == 0:
+    return "even"
+  else:
+    return "odd"
+
+#002
+def hello(str):
+  return "Hello " + str
+
+print(hello("Giorgi"))
+print(hello("Saba"))
+print(hello("Davit"))
+
+#003
+def str_concatenation(a, b):
+  return str(a) + " " + str(b)
